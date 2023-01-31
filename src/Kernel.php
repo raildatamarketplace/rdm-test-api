901174ae086ef38dfbf2c149d6ef8dd08716228f
mylesadamsonrdg <myles.adamson@raildeliverygroup.com>
2023-01-31 19:12:11 +0000

    Update Kernel.php
    Update the file so we are using temporary file storage for logs and cache in the production environment

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,22 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        if ($this->environment == 'prod') {
+            return '/tmp/var/cache';
+        }
+        
+        return dirname(__DIR__).'var/cache';
+    }
+
+    public function getLogDir(): string
+    {
+        if ($this->environment == 'prod') {
+            return '/tmp/var/log';
+        }
+        
+        return dirname(__DIR__).'var/log';
+    }
 }
