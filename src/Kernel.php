0fbf2ef01c679b737fce6cf409a5d11e3dc22c41
mylesadamsonrdg <myles.adamson@raildeliverygroup.com>
2023-01-31 19:41:55 +0000

    Update Kernel.php
    Set the folder that is required to be used for storing and reading the cache and logs on live

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,20 +10,12 @@ class Kernel extends BaseKernel
     use MicroKernelTrait;
 
     public function getCacheDir(): string
-    {
-        if ($this->environment == 'dev') {
-            return '/var/cache';
-        }
-        
+    {        
         return dirname(__DIR__).'/tmp/var/cache';
     }
 
     public function getLogDir(): string
     {
-        if ($this->environment == 'dev') {
-            return '/var/log';
-        }
-        
         return dirname(__DIR__).'/tmp/var/log';
     }
 }
